6d414c9d7a856c6c749aab10d8bccab7ff871d5a
Raúl <ralo.dev@pm.me>
2024-04-27 21:10:46 -0600

    Add Contact section Component(s)

---
+++ src/components/landing/contact/ContactForm.vue
@@ -0,0 +1,125 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import GlassButton from '@/components/GlassButton.vue'
+const WEB3FORMS_ACCESS_KEY = 'a492c371-8aa4-42fc-9f22-453715749f65'
+const name = ref('')
+const email = ref('')
+const message = ref('')
+const sending = ref(false)
+const isMessageAvailable = ref(true)
+const cooldownTimeLeft = ref('00:00')
+
+onMounted(() => {
+  checkMessageAvailable()
+})
+
+const checkMessageAvailable = () => {
+  let lastMessage = localStorage.getItem('lastMessage')
+  if (lastMessage) {
+    let lastMessageTime = new Date(lastMessage)
+    let currentTime = new Date()
+    // Calculamos la diferencia en milisegundos
+    let difference = currentTime.getTime() - lastMessageTime.getTime()
+    // Convertimos la diferencia a minutos
+    let differenceInMinutes = difference / 60000
+    if (differenceInMinutes < 30) {
+      // Convertir la diferencia de nuevo a minutos y segundos para la salida
+      let minutes = 30 - Math.floor(differenceInMinutes)
+      let seconds = 59 - Math.floor((difference % (1000 * 60)) / 1000)
+      cooldownTimeLeft.value = `${minutes}:${seconds.toString().padStart(2, '0')}`
+      // Check again in 1 sec
+      setTimeout(() => {
+        checkMessageAvailable()
+      }, 1000)
+      isMessageAvailable.value = false
+      return false
+    }
+  }
+  isMessageAvailable.value = true
+  return true
+}
+
+const submitForm = async () => {
+  if (!checkMessageAvailable()) {
+    console.error("You can't send more messages")
+    return
+  }
+  sending.value = true
+  const response = await fetch('https://api.web3forms.com/submit', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify({
+      access_key: WEB3FORMS_ACCESS_KEY,
+      name: name.value,
+      email: email.value,
+      message: message.value
+    })
+  })
+  const result = await response.json()
+  if (result.success) {
+    console.log(result)
+  }
+  let currentDateTime = new Date(Date.now()).toISOString()
+  localStorage.setItem('lastMessage', currentDateTime)
+  checkMessageAvailable()
+}
+</script>
+<template>
+  <form
+    @submit.prevent="submitForm"
+    class="flex w-full max-w-[600px] flex-col gap-y-5 self-center pt-3 text-start open"
+  >
+    <label class="block" for="name">
+      {{ $t('contact.name_label') }}
+      <input
+        type="text"
+        name="name"
+        v-model="name"
+        autocomplete="name"
+        :placeholder="$t('contact.name_placeholder')"
+        class="block px-3 min-h-[45px] w-full select-all rounded-md rounded-s-md border-[1px] bg-white outline-0 focus:outline-1 focus:outline-sky-700 border-neutral-400"
+        required
+      />
+    </label>
+    <label class="block" for="email">
+      {{ $t('contact.email_label') }}
+      <input
+        type="email"
+        name="email"
+        v-model="email"
+        autocomplete="email"
+        :placeholder="$t('contact.email_placeholder')"
+        class="block px-3 min-h-[45px] w-full select-all rounded-md rounded-s-md border-[1px] bg-white outline-0 focus:outline-1 focus:outline-sky-700 border-neutral-400"
+        required
+      />
+    </label>
+    <label class="block" for="message">
+      {{ $t('contact.message_label') }}
+      <textarea
+        name="message"
+        v-model="message"
+        class="block px-3 py-2 min-h-[45px] w-full select-all rounded-md rounded-s-md border-[1px] bg-white outline-0 focus:outline-1 focus:outline-sky-700 border-neutral-400"
+        :placeholder="$t('contact.message_placeholder')"
+        required
+      ></textarea>
+    </label>
+
+    <span v-if="!isMessageAvailable" class="text-red-600 source text-sm text-center">
+      {{ $t('contact.restriction') }}
+    </span>
+
+    <span class="mx-auto">
+      <GlassButton :disabled="sending || !isMessageAvailable" type="submit">
+        <span v-if="isMessageAvailable" class="px-3">
+          {{ sending ? $t('contact.sending') : $t('contact.send') }}
+        </span>
+        <span v-else class="px-3">
+          {{ cooldownTimeLeft }}
+        </span>
+      </GlassButton>
+    </span>
+  </form>
+</template>
---
+++ src/components/landing/contact/ContactSection.vue
@@ -0,0 +1,189 @@
+<template>
+  <section
+    id="contact"
+    name="contact"
+    class="bg-[#fff] py-10 flex justify-center w-full min-h-[100vh] items-center relative"
+  >
+    <div class="custom-shape-divider-top-1713669013">
+      <svg
+        data-name="Layer 1"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1200 120"
+        preserveAspectRatio="none"
+      >
+        <path
+          d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z"
+          opacity=".25"
+          class="shape-fill"
+        ></path>
+        <path
+          d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z"
+          opacity=".5"
+          class="shape-fill"
+        ></path>
+        <path
+          d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z"
+          class="shape-fill"
+        ></path>
+      </svg>
+    </div>
+    <div class="content mt-10">
+      <h1 class="source text-5xl font-semibold text-center tracking-wider uppercase mb-10 w-full">
+        {{ $t('contact.title') }}
+      </h1>
+      <div class="flex w-full flex-col gap-y-5">
+        <p class="text-center text-lg source">
+          {{ $t('contact.description') }}
+        </p>
+        <span class="h-1 w-full bg-gradient-to-r from-transparent via-neutral-300 to-transparent" />
+        <span class="text-center">
+          <p class="text-center text-lg source">
+            {{ $t('contact.social') }}
+          </p>
+          <div class="flex w-full flex-wrap items-center justify-center gap-5 pt-3">
+            <!-- Input group -->
+            <div class="flex items-center">
+              <input
+                type="email"
+                class="pointer-events-none min-h-[45px] w-[200px] select-all rounded-s-md border-[1px] px-3 bg-gray-50"
+                id="Email"
+                name="Email"
+                :placeholder="$t('contact.email')"
+                autocomplete="off"
+                :value="$t('contact.email')"
+                readonly
+              />
+              <button
+                class="flex min-h-[45px] min-w-[85px] select-none items-center justify-center rounded-e-md border-none px-2 text-sm text-white transition-colors duration-300 ease-in-out"
+                @click="copyEmail"
+                :disabled="copied"
+                :title="$t('contact.copy')"
+                :class="
+                  copied
+                    ? 'cursor-default bg-neutral-600'
+                    : 'cursor-pointer bg-sky-800 hover:bg-sky-900 '
+                "
+              >
+                <span v-if="!copied" class="flex gap-x-1 text-base source items-center">
+                  <CopyIcon class="w-4 h-4" />
+                  {{ $t('contact.copy') }}
+                </span>
+                <span v-else class="contact@ralo.dev flex gap-x-1 text-base source items-center">
+                  <svg
+                    class="my-auto h-4 w-4 text-white"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"
+                    />
+                  </svg>
+                  {{ $t('contact.copied') }}
+                </span>
+              </button>
+            </div>
+            <a href="https://github.com/ralodev" target="_blank">
+              <GlassButton bg="bg-neutral-800" title="GitHub" type="icon">
+                <GithubIcon class="w-8 h-8" />
+              </GlassButton>
+            </a>
+            <a href="https://www.linkedin.com/in/raul-lc/" target="_blank">
+              <GlassButton bg="bg-[#0e72a3]" title="LinkedIn" type="icon">
+                <LinkedinIcon class="w-8 h-8" />
+              </GlassButton>
+            </a>
+          </div>
+        </span>
+        <span class="h-1 w-full bg-gradient-to-r from-transparent via-neutral-300 to-transparent" />
+        <div class="flex w-full flex-col text-center justify-center">
+          <p class="text-center text-lg source">
+            {{ $t('contact.form') }}
+          </p>
+          <ContactForm />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import GlassButton from '@/components/GlassButton.vue'
+import CopyIcon from '@/components/icons/CopyIcon.vue'
+import GithubIcon from '@/components/icons/GithubIcon.vue'
+import LinkedinIcon from '@/components/icons/LinkedinIcon.vue'
+import ContactForm from './ContactForm.vue'
+import { ref, watch } from 'vue'
+
+const isHuman = ref(false)
+const showRecaptcha = ref(true)
+const loadingTimeout = ref(10000)
+const ReCaptchaElement = ref(null)
+const email = 'contact@ralo.dev'
+const gotcha = ref('')
+const copied = ref(false)
+function verify() {
+  console.log('Recaptcha verified')
+  isHuman.value = true
+}
+function recaptchaVerified() {
+  verify()
+}
+function recaptchaExpired() {
+  console.log('Recaptcha expired')
+  isHuman.value = false
+}
+function recaptchaFailed() {
+  console.log('Recaptcha failed')
+}
+function recaptchaError(reason: any) {
+  console.log('Recaptcha error', reason)
+}
+
+function copyEmail() {
+  if (copied.value) return
+  navigator.clipboard.writeText(email)
+  copied.value = true
+  setTimeout(() => {
+    copied.value = false
+  }, 2000)
+}
+
+watch(
+  () => gotcha.value,
+  (newValue) => {
+    if (newValue) {
+      bot()
+    }
+  }
+)
+
+function bot() {
+  console.log('Gotcha')
+  window.location.href = 'http://news.rr.nihalnavath.com/posts/Message-Sent-02788c4c'
+}
+</script>
+
+<style scoped>
+.custom-shape-divider-top-1713669013 {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  overflow: hidden;
+  line-height: 0;
+  filter: drop-shadow(0 5px 2px rgba(0, 0, 0, 0.4));
+}
+
+.custom-shape-divider-top-1713669013 svg {
+  position: relative;
+  display: block;
+  width: calc(100% + 1.3px);
+  height: 72px;
+}
+
+.custom-shape-divider-top-1713669013 .shape-fill {
+  fill: #ececec;
+}
+</style>
